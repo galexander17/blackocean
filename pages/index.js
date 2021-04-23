c829dd0edb2206ceabe90a11bd76c9eace3416b7
galexander17 <gabriel.torres9717@gmail.com>
2021-04-22 20:52:55 -0400

    updated index.js

--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+         Creating websites, and your brand.
         </p>
 
         <div className={styles.grid}>
